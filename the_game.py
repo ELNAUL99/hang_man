ecd307c0b13f27b8420889930f0a1e597f87123c
ELNAUL99 <58483543+ELNAUL99@users.noreply.github.com>
2022-04-08 16:10:26 +0200

    Update the_game.py

--- the_game.py
+++ the_game.py
@@ -3,41 +3,48 @@ from art import stages, logo
 import random
 
 print(logo)
-game=True
-word=RandomWords()
-chosen_word=word.get_random_word()
-lives=6
-already_guessed_letter=[]
-displayed_word=[]
-for i in range(len(chosen_word)):
-    displayed_word.append("_")
+end_the_program = False
 
-print(f"The word has {len(chosen_word)} letters.")
+while not end_the_program:
+	game=True
+	word=RandomWords()
+	chosen_word=word.get_random_word()
+	lives=6
+	already_guessed_letter=[]
+	displayed_word=[]
+	for i in range(len(chosen_word)):
+	    displayed_word.append("_")
 
-while game:
-	guess = input("Guess a letter: ").lower()
+	print(f"The word has {len(chosen_word)} letters.")
 
-	if guess in already_guessed_letter:
-		print("You already guessed this letter.")
-	else:
-	    for position in range(len(chosen_word)):
-	        letter = chosen_word[position]
-	        if letter == guess:
-	            displayed_word[position] = letter
+	while game:
+		guess = input("Guess a letter: ").lower()
 
-	if guess not in chosen_word:
-		print(f"{guess} is not in the word.")
-		lives-=1
+		if guess in already_guessed_letter:
+			print("You already guessed this letter.")
+		elif guess not in chosen_word:
+			print(f"{guess} is not in the word.")
+			lives-=1 
+			already_guessed_letter.append(guess)
+		else:
+		    for position in range(len(chosen_word)):
+		        letter = chosen_word[position]
+		        if letter == guess:
+		            displayed_word[position] = letter
+		    already_guessed_letter.append(guess)
 
-	print(f"{' '.join(displayed_word)}")
+		print(f"{' '.join(displayed_word)}")
 
-	if "_" not in displayed_word:
-		game = False
-		print(f"The word is {chosen_word}")
-		print("You win.")
-	elif lives == 0:
-		print(stages[lives])
-		game = False
-		print("You lose.")	
-	else:
-		print(stages[lives])
+		if "_" not in displayed_word:
+			game = False
+			print(f"The word is {chosen_word}")
+			print("You win.")
+		elif lives == 0:
+			print(stages[lives])
+			game = False
+			print(f"You lose. The word is {chosen_word.upper()}")	
+		else:
+			print(stages[lives])
+	if input("Do you want to play again? (Y/N)") != "Y":
+		end_the_program = True
+		print("Bye")
